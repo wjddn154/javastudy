a1adc919206f84ce85a22c9c26baa3d620113e28
wjddn154 <gjwjddn154@gmail.com>
2021-09-28 17:13:52 +0900

    test commit

--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -6,7 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	private static final int PORT = 6000;
+	private static final int PORT = 8000;
 	
 	public static void main(String[] args) {
 		
